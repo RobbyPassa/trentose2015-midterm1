227e5e2c8cdd3e81ecd9bf5ad4b97bde65f2ef04
Roberto <roberto.passatempi@studenti.unitn.it>
2015-11-05 10:28:34 +0100

    added exam

--- app.js
+++ app.js
@@ -9,13 +9,56 @@
  *  }
  */ 
 
-var tmpl = ' <li id="ID">' +
+var tmpl = ' <li id="ID" class="current">' +
            '  <h3>SENTENCE</h3>' +
            ' </li> ';
 
+var punteggio=0;
+var current;
+
 $(document).ready(function(){
 
-  
+      for (var i = 0; i < data.length; i++) {
+            var add_to_ul = tmpl.replace("ID", i)                                 
+                                .replace("SENTENCE", data[i].phrase_en);
+            
+            $(".sentences").replaceWith(add_to_ul);
+            $('.sentences').removeClass('sentences').addClass('current');
+            console.log(i);
+            current=0;
+          
+          while(current===0){
+          
+            var currentID = i;    //creo una variabile in cui mi salvo l'id corrente
+            var currentText = $('.options').attr("text");
+
+            //alert(data[i].phrase_en);
+            
+            $('.opt-continue').on('click', function(){   //function onclick
+                    
+                alert(i);
+            
+                if(currentText === data[currentID].phrase_de){
+                    
+                    punteggio++;
+                    
+                }
+                current++;
+            
+                /*alert(currentID);
+                
+                alert(currentText);*/
+                //if(){
+
+                  //  $('.candrink').text("You can't drink!");
+                   // $('.candrink').removeClass("candrink").addClass('nodrink'); //rimuovo la classe candrink
+               // }
+
+            })
+          }
+    }
+    
+    
 });
 
 
